9a0f716e0b6a74d1cb0f0de301dfd257c57ea503
Nick Sparrow <nikitosikinv@mail.ru>
2021-10-04 10:40:38 +0300

    запрос

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial/>
+  <div>
+    {{ hello }}
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from "axios";
+export default {
+  data() {
+    return {
+      hello: null
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("http://localhost:1337/phrase");
+      this.hello = response.data.hello;
+    } catch (error) {
+      this.error = error;
+      console.log(this.error);
+    }
+  }
+}
 </script>
