ebf07ba1a6baec67467928a5a43e7d2715c62b6d
Nick Sparrow <nikitosikinv@mail.ru>
2021-10-04 10:23:12 +0300

    добавление папок

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8abf25cfaee7c92776930bdf13703cfb'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '75ab8f36-7f35-41d6-9d7d-cd0d764b4851'
+};
